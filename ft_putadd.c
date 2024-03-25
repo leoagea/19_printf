16695b5f17329b8f04391f84538e473727c86e53
Leo Agea <leoagea@gmail.com>
2024-03-26 00:12:09 +0100

    putadd %p

--- ft_putadd.c
+++ ft_putadd.c
@@ -1,27 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putadd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/25 22:27:08 by lagea             #+#    #+#             */
-/*   Updated: 2024/03/26 00:11:54 by lagea            ###   ########.fr       */
+/*   Created: 2024/03/26 00:01:34 by lagea             #+#    #+#             */
+/*   Updated: 2024/03/26 00:11:56 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+#include "ft_printf.h"
 
-#include <unistd.h>
-#include <stdlib.h>
-
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putdec(long nb);
-void	ft_putudec(unsigned long n);
-void	ft_puthex_lower(long n);
-void	ft_puthex_upper(long n);
-void	ft_putadd(void *add);
-
-#endif
+void	ft_putadd(void *add)
+{
+	if (!add)
+		ft_putstr("(null)");
+	ft_putstr("0x");
+	ft_puthex_lower((long)add);
+}
