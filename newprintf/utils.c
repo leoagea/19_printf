2b6c6e198366c897e1da91b314e8bb0e67aaef8e
Leo Agea <leoagea@gmail.com>
2024-04-18 22:39:23 +0200

    print_string OK

--- newprintf/utils.c
+++ newprintf/utils.c
@@ -6,7 +6,7 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:27:13 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/18 22:23:52 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/18 22:39:08 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,26 @@ int	print_char(char c)
 	return 0;
 }
 
-// int main()
-// {
-// 	printf("%d\n",print_char('c'));
-// }
+int print_string(char *s)
+{
+	int	i;
+
+	i = -1;
+	while(s[++i])
+	{
+		if (ft_isprint((char)s[i]))
+			write(1, &s[i], 1);
+		else
+			return 0;
+	}
+	return 1;
+}
+
+
+int main()
+{
+	// printf("%d\n",print_char('c'));
+	// printf("%d\n",print_string("\ntest de string"));
+	// printf("%s\n","test de string");
+	
+}
