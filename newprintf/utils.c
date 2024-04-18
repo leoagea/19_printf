8908997fcd5635521542ef121bbbebeb4a4ece2e
Leo Agea <leoagea@gmail.com>
2024-04-18 23:54:02 +0200

    print_hex OK

--- newprintf/utils.c
+++ newprintf/utils.c
@@ -6,40 +6,98 @@
 /*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:27:13 by lagea             #+#    #+#             */
-/*   Updated: 2024/04/18 22:39:08 by lagea            ###   ########.fr       */
+/*   Updated: 2024/04/18 23:50:01 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/printf.h"
 #include "../inc/libft.h"
+#include "../inc/printf.h"
 
 int	print_char(char c)
 {
 	if (ft_isprint((char)c))
-		return write(1, &c, 1);
-	return 0;
+		return (write(1, &c, 1));
+	return (0);
 }
 
-int print_string(char *s)
+int	print_string(char *s)
 {
 	int	i;
 
 	i = -1;
-	while(s[++i])
+	if (!s)
+		print_string("(null)");
+	else
 	{
-		if (ft_isprint((char)s[i]))
-			write(1, &s[i], 1);
-		else
-			return 0;
+		while (s[++i])
+		{
+			if (ft_isprint((char)s[i]))
+				write(1, &s[i], 1);
+			else
+				return (0);
+		}
 	}
-	return 1;
+	return (1);
 }
 
-
-int main()
+int	print_decimal(int n)
 {
-	// printf("%d\n",print_char('c'));
-	// printf("%d\n",print_string("\ntest de string"));
-	// printf("%s\n","test de string");
+	ft_putnbr_fd(n, 1);
+	return (1);
+}
+
+int	print_hex_lower(long n)
+{
+	char	*base_16;
+
+	base_16 = "0123456789abcdef";
+	if (n < 16)
+		return (print_char(base_16[n]));
+	else
+	{
+		if (print_hex_lower(n / 16))
+			return (print_char(base_16[n % 16]));
+	}
+	return (0);
+}
+
+int	print_hex_upper(long n)
+{
+	char	*base_16;
+
+	base_16 = "0123456789ABCDEF";
+	if (n < 16)
+		return (print_char(base_16[n]));
+	else
+	{
+		if (print_hex_upper(n / 16))
+			return (print_char(base_16[n % 16]));
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char *s = NULL;
+	char *s1 = "\nTest\n";
+	long long n = -9223372036854775807;
+	int nb = 1010;
+
+	// // printf("\n%p\n",&s);
+	// printf("\n%p\n",&n);
+	// // print_add(&s);
+	// print_add(&n);
+
+	// printf("%s\n",s);
+	// printf("%s\n",s1);
+	// print_string(s);
+	// print_string(s1);
+	
+	// printf("\n");
+	// printf("%d", print_hex_lower((int)nb));
+
+	// printf("\n");
+	// printf("%d", print_hex_upper((int)nb));
+
 	
 }
